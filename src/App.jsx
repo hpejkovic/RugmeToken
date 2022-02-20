f9e06dced8c11306683c5419745c6360eb4b403b
LukaCular <luka.cular@optimitas.hr>
2022-02-20 21:53:23 +0100

    c

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import { useState, useEffect } from 'react';
 
 function App() {
   const [count, setCount] = useState(0);
-
+ 
   const IncrementTimer = () => {
     setCount(count + 1);
   };
+
+
 
   useEffect(() => {
     const timer = setInterval(() => {
@@ -18,16 +20,38 @@ function App() {
     let seconds = 0;
     let minutes = 0;
     let hours = 0;
-    let days = Math.ceil(count / 86400) % 86400;
+    const startDate = new Date(2022, 1, 18, 5, 0, 0, 0);
+    const currDate = new Date(Date.now());
+  
+    const timeDifference = Math.floor((currDate - startDate) / 1000);
 
-    seconds = count % 60;
-    minutes = Math.floor(count / 60) % 60;
-    hours = Math.floor(count / 3600) % 3600;
+    console.log(timeDifference)
+  
 
-    return `${hours.toString().padStart(2, '0')}:${minutes
+    let days = Math.floor(timeDifference / 86400) % 86400;
+
+    seconds = timeDifference % 60;
+    minutes = Math.floor(timeDifference / 60) % 60;
+    hours = Math.floor(timeDifference / 3600) % 24;
+
+    return `${days.toString().padStart(2, '0')}:${hours.toString().padStart(2, '0')}:${minutes
       .toString()
-      .padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+      .padStart(2, '0')}`;
   };
+
+  const getSeconds = () => {
+    const startDate = new Date(2022, 1, 18, 5, 0, 0, 0);
+    const currDate = new Date(Date.now());
+  
+    const timeDifference = Math.floor((currDate - startDate) / 1000);
+
+  
+
+
+    let seconds = timeDifference % 60;
+
+    return `:${seconds.toString().padStart(2, '0')}`
+  }
 
   const getDay = () => {
     let days = Math.ceil(count / 86400) % 86400;
@@ -36,8 +60,8 @@ function App() {
   //setTimeout(IncrementTimer, 1000);
   return (
     <div className="Counter">
-      {/* <p className="txt-c">{getDay()}</p> */}
-      <p>{getTime()}</p>
+      <p className="txt-c bg-black">Countdown to rug</p>
+      <p className='txt-c timer'>{getTime()}<span className='lower-Font'>{getSeconds()}</span></p>
     </div>
   );
 }
