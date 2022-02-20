e0e0afd8478a0b2339557f7cc1b81df5d407c5c8
Hrvoje Pejkovic <hpejkovic@gmail.com>
2022-02-20 14:17:04 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import { useState, useEffect } from 'react';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const IncrementTimer = () => {
+    setCount(count + 1);
+  };
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      IncrementTimer();
+    }, 1000);
+    return () => clearInterval(timer);
+  }, [count]);
+
+  const getTime = () => {
+    let seconds = 0;
+    let minutes = 0;
+    let hours = 0;
+    let days = Math.ceil(count / 86400) % 86400;
+
+    seconds = count % 60;
+    minutes = Math.ceil(count / 60) % 60;
+    hours = Math.ceil(count / 3600) % 3600;
+
+    return `${hours.toString().padStart(2, '0')}:${minutes
+      .toString()
+      .padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+  };
+
+  const getDay = () => {
+    let days = Math.ceil(count / 86400) % 86400;
+    return `${days} day`;
+  };
+  //setTimeout(IncrementTimer, 1000);
+  return (
+    <div className="Counter">
+      {/* <p className="txt-c">{getDay()}</p> */}
+      <p>{getTime()}</p>
+    </div>
+  );
+}
+
+export default App;
